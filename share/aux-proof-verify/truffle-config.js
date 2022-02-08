3fde5dc08ebaf8ec2e9e8667b8d1d6d13e62ccdc
Mikhail Komarov <nemo@nil.foundation>
2022-02-08 09:37:15 +0300

    Verification part transcript updated. Example Web3.js usage introduced.

--- share/aux-proof-verify/truffle-config.js
+++ share/aux-proof-verify/truffle-config.js
@@ -45,7 +45,7 @@ module.exports = {
             host: "127.0.0.1",     // Localhost (default: none)
             port: 8545,            // Standard Ethereum port (default: none)
             network_id: "*",       // Any network (default: none)
-        }
+        },
         // Another network with more advanced options...
         // advanced: {
         // port: 8777,             // Custom port
@@ -57,14 +57,14 @@ module.exports = {
         // },
         // Useful for deploying to a public network.
         // NB: It's important to wrap the provider as a function.
-        // ropsten: {
-        // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-        // network_id: 3,       // Ropsten's id
-        // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-        // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-        // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-        // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-        // },
+        ropsten: {
+            provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/6f3d827e1a7241859cf304c63a4f3167`),
+            network_id: 3,       // Ropsten's id
+            gas: 5500000,        // Ropsten has a lower block limit than mainnet
+            confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+            timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+            skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+        }
         // Useful for private networks
         // private: {
         // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
@@ -84,11 +84,11 @@ module.exports = {
             version: "0.8.11",    // Fetch exact version from solc-bin (default: truffle's version)
             // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
             settings: {          // See the solidity docs for advice about optimization and evmVersion
-             optimizer: {
-               enabled: true,
-               runs: 1500
-             },
-             evmVersion: "constantinople"
+                optimizer: {
+                    enabled: true,
+                    runs: 1500
+                },
+                evmVersion: "constantinople"
             }
         }
     },
