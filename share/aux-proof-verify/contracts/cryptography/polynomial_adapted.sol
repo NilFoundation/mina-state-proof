95dbe0680670441a66e9ca6d5105e5b43d87f5f6
Ilias Khairullin <ilias@nil.foundation>
2022-02-23 18:04:56 +0300

    Polynomial operations updated. Lagrange interpolation added. Tests added. #41

---
+++ share/aux-proof-verify/contracts/cryptography/polynomial_adapted.sol
@@ -0,0 +1,139 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+//---------------------------------------------------------------------------//
+// Copyright (c) 2018-2021 Mikhail Komarov <nemo@nil.foundation>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//---------------------------------------------------------------------------//
+
+pragma solidity >=0.6.0;
+pragma experimental ABIEncoderV2;
+
+import '../field_math.sol';
+
+/**
+ * @title Turbo Plonk polynomial evaluation
+ * @dev Implementation of Turbo Plonk's polynomial evaluation algorithms
+ *
+ * Expected to be inherited by `TurboPlonk.sol`
+ */
+library polynomial_adapted {
+    /*
+      Computes the evaluation of a polynomial f(x) = sum(a_i * x^i) on the given point.
+      The coefficients of the polynomial are given in
+        a_0 = coefsStart[0], ..., a_{n-1} = coefsStart[n - 1]
+      where n = nCoeffs = friLastLayerDegBound. Note that coefsStart is not actually an array but
+      a direct pointer.
+      The function requires that n is divisible by 8.
+    */
+    function evaluate(
+        uint256[] memory coeffs,
+        uint256 point,
+        uint256 modulus
+    ) internal pure returns (uint256) {
+        uint256 result = 0;
+        uint256 x_pow = 1;
+        for (uint256 i = 0; i < coeffs.length; i++) {
+            assembly {
+                result := addmod(result, mulmod(x_pow, mload(add(add(coeffs, 0x20), mul(i, 0x20))), modulus), modulus)
+                x_pow := mulmod(x_pow, point, modulus)
+            }
+        }
+        return result;
+    }
+
+    function add_poly(
+        uint256[] memory a,
+        uint256[] memory b,
+        uint256 modulus
+    ) internal pure returns (uint256[] memory) {
+        uint256 len;
+        if (a.length < b.length) {
+            len = b.length;
+        } else {
+            len = a.length;
+        }
+
+        uint256[] memory results = new uint256[](len);
+
+        for (uint256 i = 0; i < len; i++) {
+            uint256 aOrZero = a.length > i ? a[i] : 0;
+            uint256 bOrZero = b.length > i ? b[i] : 0;
+            assembly {
+                mstore(add(add(results, 0x20), mul(i, 0x20)), addmod(aOrZero, bOrZero, modulus))
+            }
+        }
+
+        return results;
+    }
+
+    function mul_poly(
+        uint256[] memory a,
+        uint256[] memory b,
+        uint256 modulus
+    ) internal pure returns (uint256[] memory resultTerms) {
+        uint256 padding = 0;
+        for (uint256 i = 0; i < b.length; i++) {
+            uint256[] memory currentValues = new uint256[](a.length + padding);
+            uint256 aTerm;
+            uint256 bTerm;
+            // TODO: seems redundant
+            for (uint256 k = 0; k < padding; k++) {
+//                currentValues[k] = 0;
+                assembly {
+                    mstore(add(add(currentValues, 0x20), mul(k, 0x20)), 0)
+                }
+            }
+            for (uint256 j = 0; j < a.length; j++) {
+//                uint256 aTerm = a[j];
+//                uint256 bTerm = b[i];
+//                currentValues[j + padding] = aTerm.gf256Mul(bTerm);
+                assembly {
+                    aTerm := mload(add(add(a, 0x20), mul(j, 0x20)))
+                    bTerm := mload(add(add(b, 0x20), mul(i, 0x20)))
+                    mstore(add(add(currentValues, 0x20), mul(add(j, padding), 0x20)), mulmod(aTerm, bTerm, modulus))
+                }
+            }
+            resultTerms = add_poly(resultTerms, currentValues, modulus);
+            padding += 1;
+        }
+    }
+
+    function lagrange_interpolation(
+        uint256[] memory xs,
+        uint256[] memory fxs,
+        uint256 modulus
+    ) internal pure returns (uint256[] memory result) {
+        require(xs.length == fxs.length);
+        uint256 len = fxs.length;
+        for (uint256 i = 0; i < len; i++) {
+            uint256[] memory thisPoly = new uint256[](1);
+            thisPoly[0] = 1;
+            for (uint256 j = 0; j < len; j++) {
+                if (i == j) {
+                    continue;
+                }
+                uint256 denominator = field_math.fsub(xs[i], xs[j], modulus);
+                uint256[] memory thisTerm = new uint256[](2);
+                thisTerm[0] = field_math.fdiv(modulus - xs[j], denominator, modulus);
+                thisTerm[1] = field_math.fdiv(uint256(1), denominator, modulus);
+                thisPoly = mul_poly(thisPoly, thisTerm, modulus);
+            }
+            if (fxs.length + 1 >= i) {
+                uint256[] memory multiple = new uint256[](1);
+                multiple[0] = fxs[i];
+                thisPoly = mul_poly(thisPoly, multiple, modulus);
+            }
+            result = add_poly(result, thisPoly, modulus);
+        }
+    }
+}
---
+++ share/aux-proof-verify/contracts/field_math.sol
@@ -0,0 +1,94 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+//---------------------------------------------------------------------------//
+// Copyright (c) 2021 Mikhail Komarov <nemo@nil.foundation>
+// Copyright (c) 2021 Ilias Khairullin <ilias@nil.foundation>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//---------------------------------------------------------------------------//
+pragma solidity >=0.6.0;
+pragma experimental ABIEncoderV2;
+
+/**
+ * @title Bn254 elliptic curve crypto
+ * @dev Provides some basic methods to compute bilinear pairings, construct group elements and misc numerical methods
+ */
+library field_math {
+    // Perform a modular exponentiation. This method is ideal for small exponents (~64 bits or less), as
+    // it is cheaper than using the pow precompile
+    function pow_small(uint256 base, uint256 exponent, uint256 modulus) internal pure returns (uint256) {
+        uint256 result = 1;
+        uint256 input = base;
+        uint256 count = 1;
+
+        assembly {
+            let endpoint := add(exponent, 0x01)
+            for {} lt(count, endpoint) {count := add(count, count)}
+            {
+                if and(exponent, count) {
+                    result := mulmod(result, input, modulus)
+                }
+                input := mulmod(input, input, modulus)
+            }
+        }
+
+        return result;
+    }
+
+    /// @dev Modular inverse of a (mod p) using euclid.
+    /// 'a' and 'p' must be co-prime.
+    /// @param a The number.
+    /// @param p The mmodulus.
+    /// @return x such that ax = 1 (mod p)
+    function invmod(uint256 a, uint256 p) internal pure returns (uint256) {
+        require(a != 0 && a != p && p != 0);
+        if (a > p)
+            a = a % p;
+        int256 t1;
+        int256 t2 = 1;
+        uint256 r1 = p;
+        uint256 r2 = a;
+        uint256 q;
+        while (r2 != 0) {
+            q = r1 / r2;
+            (t1, t2, r1, r2) = (t2, t1 - int256(q) * t2, r2, r1 - q * r2);
+        }
+        if (t1 < 0)
+            return (p - uint256(-t1));
+        return uint256(t1);
+    }
+
+    function fadd(uint256 a, uint256 b, uint256 modulus) internal pure returns (uint256 result) {
+        assembly {
+            result := addmod(a, b, modulus)
+        }
+    }
+
+    function fsub(uint256 a, uint256 b, uint256 modulus) internal pure returns (uint256 result) {
+        assembly {
+            result := addmod(a, sub(modulus, b), modulus)
+        }
+    }
+
+    function fmul(uint256 a, uint256 b, uint256 modulus) internal pure returns (uint256 result) {
+        assembly {
+            result := mulmod(a, b, modulus)
+        }
+    }
+
+    function fdiv(uint256 a, uint256 b, uint256 modulus) internal pure returns (uint256 result) {
+        uint256 b_inv = invmod(b, modulus);
+        assembly {
+            result := mulmod(a, b_inv, modulus)
+        }
+    }
+}
