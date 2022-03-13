adba50919899e1fb4c0a7f33842b8a0d7bc269af
Mikhail Komarov <nemo@nil.foundation>
2022-03-13 20:50:56 +0300

    Submodules updated. Proof generator updated.

--- bin/aux-proof-gen/src/main.cpp
+++ bin/aux-proof-gen/src/main.cpp
@@ -29,27 +29,64 @@
 #include <nil/crypto3/algebra/curves/params/multiexp/alt_bn128.hpp>
 #include <nil/crypto3/algebra/curves/params/wnaf/alt_bn128.hpp>
 
-#include <nil/crypto3/zk/components/blueprint.hpp>
-#include <nil/crypto3/zk/components/hashes/plonk/poseidon_15_wires.hpp>
-#include <nil/crypto3/zk/components/algebra/curves/plonk/variable_base_endo_scalar_mul_15_wires.hpp>
+#include <nil/crypto3/zk/blueprint/plonk.hpp>
+
+#include <nil/crypto3/zk/components/hashes/poseidon/plonk/poseidon_15_wires.hpp>
+#include <nil/crypto3/zk/components/algebra/curves/edwards/plonk/variable_base_endo_scalar_mul_15_wires.hpp>
 
 #include <nil/crypto3/hash/algorithm/hash.hpp>
+#include <nil/crypto3/hash/keccak.hpp>
 #include <nil/crypto3/hash/sha2.hpp>
 
 #include <nil/crypto3/pubkey/algorithm/sign.hpp>
 #include <nil/crypto3/pubkey/eddsa.hpp>
 
-#include <nil/crypto3/zk/snark/algorithms/generate.hpp>
+#include <nil/crypto3/zk/algorithms/generate.hpp>
 
 #include <nil/crypto3/zk/snark/systems/plonk/pickles/proof.hpp>
 #include <nil/crypto3/zk/snark/systems/plonk/redshift/prover.hpp>
 #include <nil/crypto3/zk/snark/systems/plonk/redshift/preprocessor.hpp>
-#include <nil/crypto3/zk/snark/relations/non_linear_combination.hpp>
+
+#include <nil/crypto3/zk/math/non_linear_combination.hpp>
 
 #include <nil/mina/auxproof/sexp.hpp>
 
 using namespace nil;
 using namespace nil::crypto3;
+
+typedef algebra::curves::alt_bn128<254> curve_type;
+typedef typename curve_type::base_field_type field_type;
+constexpr static const std::size_t m = 2;
+constexpr static const std::size_t k = 1;
+
+constexpr static const std::size_t table_rows_log = 4;
+constexpr static const std::size_t table_rows = 1 << table_rows_log;
+constexpr static const std::size_t permutation_size = 4;
+constexpr static const std::size_t usable_rows = 1 << table_rows_log;
+
+struct redshift_params {
+    using merkle_hash_type = hashes::keccak_1600<512>;
+    using transcript_hash_type = hashes::keccak_1600<512>;
+
+    constexpr static const std::size_t witness_columns = 3;
+    constexpr static const std::size_t public_input_columns = 1;
+    constexpr static const std::size_t constant_columns = 0;
+    constexpr static const std::size_t selector_columns = 2;
+
+    constexpr static const std::size_t lambda = 40;
+    constexpr static const std::size_t r = table_rows_log - 1;
+    constexpr static const std::size_t m = 2;
+};
+
+constexpr static const std::size_t table_columns =
+    redshift_params::witness_columns + redshift_params::public_input_columns;
+
+typedef zk::commitments::fri<field_type, redshift_params::merkle_hash_type, redshift_params::transcript_hash_type, m>
+    fri_type;
+
+typedef zk::snark::redshift_params<field_type, redshift_params::witness_columns, redshift_params::public_input_columns,
+                                   redshift_params::constant_columns, redshift_params::selector_columns>
+    circuit_params;
 
 int main(int argc, char *argv[]) {
 #ifndef __EMSCRIPTEN__
@@ -73,7 +110,7 @@ int main(int argc, char *argv[]) {
         return 0;
     }
 
-    std::string err{};
+    std::string err {};
     sexp s;
     if (vm.count("proof")) {
         if (boost::filesystem::exists(vm["proof"].as<std::string>())) {
@@ -89,7 +126,6 @@ int main(int argc, char *argv[]) {
         s = parse(string, err);
     }
 
-
 #else
     std::string string;
     std::cin >> string;
@@ -97,29 +133,31 @@ int main(int argc, char *argv[]) {
 #endif
 
     if (!err.empty()) {
-
     }
 
-    typedef algebra::curves::alt_bn128<254> curve_type;
-    using TBlueprintField = typename curve_type::base_field_type;
-    constexpr std::size_t WiresAmount = 15;
     constexpr typename curve_type::template g1_type<>::value_type B = curve_type::template g1_type<>::value_type::one();
-    using TArithmetization = zk::snark::plonk_constraint_system<TBlueprintField, WiresAmount>;
+    using ArithmetizationType = zk::snark::plonk_constraint_system<field_type>;
 
-    zk::snark::pickles_proof<curve_type, WiresAmount> state_proof;
+    zk::snark::pickles_proof<curve_type, redshift_params::witness_columns> state_proof;
 
-    zk::components::blueprint<TArithmetization> bp;
+    zk::blueprint<ArithmetizationType> bp;
+    zk::blueprint_private_assignment_table<ArithmetizationType, redshift_params::witness_columns> private_assignment;
+    zk::blueprint_public_assignment_table<ArithmetizationType, redshift_params::public_input_columns,
+                                          redshift_params::constant_columns, redshift_params::selector_columns>
+        public_assignment;
 
-    zk::components::element_g1_variable_base_endo_scalar_mul<TArithmetization, curve_type> scalar_mul_component(bp);
-    zk::components::poseidon_plonk<TArithmetization, curve_type> poseidon_component(bp);
+    zk::components::curve_element_variable_base_endo_scalar_mul<ArithmetizationType, curve_type, 0, 1, 2, 3, 4, 5, 6, 7,
+                                                                8, 9, 10, 11, 12, 13, 14>
+        scalar_mul_component(bp);
+    zk::components::poseidon_plonk<ArithmetizationType, curve_type> poseidon_component(bp);
 
-    scalar_mul_component.generate_gates();
+    scalar_mul_component.generate_gates(public_assignment);
     poseidon_component.generate_gates();
 
     typename curve_type::scalar_field_type::value_type a = curve_type::scalar_field_type::value_type::one();
     typename curve_type::template g1_type<>::value_type P = curve_type::template g1_type<>::value_type::one();
 
-    scalar_mul_component.generate_assignments(a, P);
+    scalar_mul_component.generate_assignments(private_assignment, public_assignment, {P, a});
     poseidon_component.generate_assignments();
 
     auto cs = bp.get_constraint_system();
