dd2424055db705ce4e4f6a3617d3c0a1ecc4164e
Mikhail Komarov <nemo@nil.foundation>
2022-03-15 19:34:52 +0300

    Polygon deployment settings introduced.

--- share/aux-proof-verify/truffle-config.js
+++ share/aux-proof-verify/truffle-config.js
@@ -64,6 +64,30 @@ module.exports = {
             confirmations: 2,    // # of confs to wait between deployments. (default: 0)
             timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
             skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+        },
+
+        // Another network with more advanced options...
+        // advanced: {
+        // port: 8777,             // Custom port
+        // network_id: 1342,       // Custom network
+        // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
+        // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
+        // from: <address>,        // Account to send txs from (default: accounts[0])
+        // websocket: true        // Enable EventEmitter interface for web3 (default: false)
+        // },
+        // Useful for deploying to a public network.
+        // NB: It's important to wrap the provider as a function.
+        mumbai: {
+            // provider: () => new HDWalletProvider(mnemonic, `https://rpc-mumbai.maticvigil.com`),
+            // provider: () => new HDWalletProvider(mnemonic, `https://rpc-mumbai.matic.today`),
+            // provider: () => new HDWalletProvider(mnemonic, `https://matic-mumbai.chainstacklabs.com`),
+            provider: () => new HDWalletProvider(mnemonic, `https://matic-testnet-archive-rpc.bwarelabs.com 
+`),
+            network_id: 80001,       // Mumbai's id
+            gas: 5500000,        // Ropsten has a lower block limit than mainnet
+            confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+            timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+            skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
         }
         // Useful for private networks
         // private: {
