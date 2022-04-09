5b678a1210ea16d5e9024567c71892778de83269
Ilias Khairullin <ilias@nil.foundation>
2022-04-09 18:12:02 +0300

    Test API unified addition component verifier updated. #41

--- share/aux-proof-verify/contracts/redshift/test/public_api_redshift_unified_addition.sol
+++ share/aux-proof-verify/contracts/redshift/test/public_api_redshift_unified_addition.sol
@@ -23,51 +23,52 @@ import '../proof_map_parser.sol';
 import '../verifier_unified_addition_component.sol';
 
 contract TestRedshiftVerifierUnifiedAddition {
-    types.lpc_params_type lpc_params;
-    types.redshift_common_data common_data;
+    types.lpc_params_type m_lpc_params;
+    types.redshift_common_data m_common_data;
+    bool m_result;
 
     function set_initial_params(uint256 modulus, uint256 r, uint256 max_degree, uint256 lambda, uint256 m, uint256 rows_amount, uint256 omega, uint256 columns_number) public {
-        lpc_params.modulus = modulus;
-        lpc_params.lambda = lambda;
-        lpc_params.r = r;
-        lpc_params.m = m;
+        m_lpc_params.modulus = modulus;
+        m_lpc_params.lambda = lambda;
+        m_lpc_params.r = r;
+        m_lpc_params.m = m;
 
-        lpc_params.fri_params.modulus = modulus;
-        lpc_params.fri_params.r = r;
-        lpc_params.fri_params.max_degree = max_degree;
+        m_lpc_params.fri_params.modulus = modulus;
+        m_lpc_params.fri_params.r = r;
+        m_lpc_params.fri_params.max_degree = max_degree;
 
-        common_data.rows_amount = rows_amount;
-        common_data.omega = omega;
-        common_data.columns_rotations = new int256[][](columns_number);
+        m_common_data.rows_amount = rows_amount;
+        m_common_data.omega = omega;
+        m_common_data.columns_rotations = new int256[][](columns_number);
     }
 
     function set_U(uint256[] calldata U) public {
-        lpc_params.fri_params.U = U;
+        m_lpc_params.fri_params.U = U;
     }
 
     function set_V(uint256[] calldata V) public {
-        lpc_params.fri_params.V = V;
+        m_lpc_params.fri_params.V = V;
     }
 
     function set_D_omegas(uint256[] calldata D_omegas) public {
-        lpc_params.fri_params.D_omegas = D_omegas;
+        m_lpc_params.fri_params.D_omegas = D_omegas;
     }
 
     function set_q(uint256[] calldata q) public {
-        lpc_params.fri_params.q = q;
+        m_lpc_params.fri_params.q = q;
     }
 
     function set_column_rotations(int256[] calldata rotations, uint256 i) public {
-        common_data.columns_rotations[i] = rotations;
+        m_common_data.columns_rotations[i] = rotations;
     }
 
-    function verify(bytes calldata blob) public view {
+    function verify(bytes calldata blob) public {
         (types.redshift_proof_map memory proof_map, uint256 proof_size) = redshift_proof_map_parser.parse_be(blob, 0);
         require(proof_size == blob.length, "Proof length was detected incorrectly!");
         bytes memory init_blob = hex"";
         types.transcript_data memory tr_state;
         transcript.init_transcript(tr_state, init_blob);
-        bool result = redshift_verifier_unified_addition_component.parse_verify_proof_be(blob, 0, tr_state, proof_map, lpc_params, common_data);
-        require(result, "Proof is not correct!");
+        m_result = redshift_verifier_unified_addition_component.parse_verify_proof_be(blob, 0, tr_state, proof_map, m_lpc_params, m_common_data);
+        require(m_result, "Proof is not correct!");
     }
 }
