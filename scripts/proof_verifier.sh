91d2e449ba4d4edccc64f65017ddb98c4f2e6dd6
SK0M0R0H <i.shirobokov@nil.foundation>
2022-10-27 21:48:09 +0300

    proof verifier update

--- scripts/proof_verifier.sh
+++ scripts/proof_verifier.sh
@@ -8,8 +8,8 @@ PROOF_PATH="${BASEDIR}/share/aux-proof-verify/proof.data"
 ADDRESS_PATH="${BASEDIR}/share/aux-proof-verify/address.data"
 VERIFIER=${BASEDIR}/share/aux-proof-verify/web3_verify.py
 DEPLOY=${BASEDIR}/share/aux-proof-verify/web3_deploy.py
-PROOF_FETCHER=${BASEDIR}/share/proof-market/scripts/proof_get_id.py
-PROOF_ID="1"
+PROOF_FETCHER=${BASEDIR}/share/proof-market/proof_get_id.py
+PROOF_ID="8"
 echo getting proof...
 python3 $PROOF_FETCHER $PROOF_ID $PROOF_PATH
 echo deploy...
@@ -17,4 +17,4 @@ python3 $DEPLOY $ADDRESS_PATH
 ADDRESS_VALUE=$(<$ADDRESS_PATH)
 echo $ADDRESS_VALUE
 echo verify...
-#python3 $VERIFIER $PROOF_PATH
+python3 $VERIFIER $ADDRESS_VALUE $PROOF_PATH
