c35f859cb2e54547ca451994eb087c933d28833c
Mikhail Komarov <nemo@nil.foundation>
2022-11-29 21:38:27 +0200

    More commit log branch selection rule updates.

--- share/evm-state/contracts/selection.sol
+++ share/evm-state/contracts/selection.sol
@@ -55,14 +55,86 @@ library selection {
             }
         }
     }
-}
 
-/// Checks whether the fork is short range wrt to candidate chain
-    function is_short_range(selection candidate) returns (bool);
+    function check_consensus_state(state.consensus s1, state.consensus s2, uint32 s2_epoch_slot) returns (bool) {
+        if (s1.epoch_count == s2.epoch_count + 1 && s2_epoch_slot >= self.config().slots_per_epoch * 2 / 3) {
+            // S1 is one epoch ahead of S2 and S2 is not in the seed update range
+            return s1.staking_epoch_data.lock_checkpoint == s2.next_epoch_data.lock_checkpoint;
+        } else {
+            return false;
+        }
+    }
 
-/// Calculates the relate minimum window density wrt to candidate chain.
-    function relative_min_window_density(selection candidate) returns (uint32);
+    /// Checks whether the fork is short range wrt to candidate chain
+    function is_short_range(selection candidate) returns (bool) {
+        state.consensus a = consensus_state();
+        state.consensus b = candidate.consensus_state;
+        uint256 a_prev_lock_checkpoint = a.staking_epoch_data.lock_checkpoint;
+        uint256 b_prev_lock_checkpoint = b.staking_epoch_data.lock_checkpoint;
 
-/// Constants used for consensus
+        if (a.epoch_count == b.epoch_count) {
+            // Simple case: blocks have same previous epoch, so compare previous epochs' lock_checkpoints
+            return a_prev_lock_checkpoint == b_prev_lock_checkpoint;
+        } else {
+            // Check for previous epoch case using both orientations
+            return check_consensus_state(a, b, candidate.epoch_slot()) || check_consensus_state(b, a, self.epoch_slot());
+        }
+    }
+
+    function max(uint256 a, uint256 b) external pure returns (uint256) {
+        return a >= b ? a : b;
+    }
+
+    function min(uint256 a, uint256 b) external pure returns (uint256) {
+        return a <= b ? a : b;
+    }
+
+    function compute_projective_window(selection tip_state) returns (uint256[]) {
+        // compute shift count
+        uint256 shift_count = min(max(max_slot - tip_state.curr_global_slot.slot_number.saturating_sub(1), 0), config()
+            .sub_windows_per_window);
+        // initialize projected window based off of chain_a
+        uint256[] projected_window = tip_state.sub_window_densities;
+
+        // relative sub window
+        uint256 rel_sub_window = tip_state.curr_global_slot.slot_number / config().sub_windows_per_window % config().sub_windows_per_window;
+
+        // ring shift
+        while (shift_count > 0) {
+            rel_sub_window = (rel_sub_window + 1) % config().sub_windows_per_window;
+            if (projected_window[rel_sub_window]) {
+                projected_window[rel_sub_window] = 0;
+            }
+            shift_count -= 1;
+        }
+
+        return projected_window;
+    }
+
+    /// Calculates the relate minimum window density wrt to candidate chain.
+    function relative_min_window_density(selection candidate) returns (uint32) {
+        state.consensus tip_state = consensus_state();
+        let chain_b = candidate.consensus_state();
+
+        uint256 max_slot = max(tip_state.curr_global_slot.slot_number, chain_b.curr_global_slot.slot_number);
+
+        // grace-period rule
+        if (max_slot < config().grace_period_end) {
+            return tip_state.min_window_density;
+        }
+
+        uint256[] projected_window = compute_projective_window();
+
+        // compute projected window density
+        uint256 projected_window_density = 0;
+        for (uint256 itr = 0; itr < projected_window.length; itr++) {
+            projected_window_density + projected_window[itr];
+        }
+
+        // compute minimum window density
+        return min(tip_state.min_window_density, projected_window_density);
+    }
+
+    /// Constants used for consensus
     function config() returns (consensus_constants constants);
 }
