5aaa1a814db6178c4a610d9104888c4bcf903b8e
vitalylativ <vitalylativ@yandex.ru>
2023-07-28 13:58:48 +0800

    Added custom verifier interface

--- contracts/interfaces/ICustomVerifier.sol
+++ contracts/interfaces/ICustomVerifier.sol
@@ -18,43 +18,9 @@
 //---------------------------------------------------------------------------//
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-import '@nilfoundation/evm-placeholder-verification/contracts/interfaces/verifier.sol';
-import "../interfaces/ICustomVerifier.sol";
-
-
-contract AccountPathVerifier is ICustomVerifier, Ownable {
-
-    address _verifier;
-    address _gates;
-    uint256[] _init_params;
-    int256[][] _columns_rotations;
-
-    constructor(
-        address verifier,
-        address gates,
-        uint256[] memory init_params,
-        int256[][] memory columns_rotations        
-    ) {
-        _verifier = verifier;
-        _gates = gates;
-        _init_params = init_params;
-        _columns_rotations = columns_rotations;
-    }
-
-    function setVerifier(address verifier) external onlyOwner {
-        _verifier = verifier;
-    }
-
-    function setGates(address gates) external onlyOwner {
-        _gates = gates;
-    }
-
+interface ICustomVerifier  {
     function verify(
         bytes calldata blob,
         uint256[] calldata public_input
-    ) external view returns (bool) {
-        IVerifier v = IVerifier(_verifier);
-        return v.verify(blob, _init_params, _columns_rotations, _gates);
-    }
+    ) external view returns (bool);
 }
--- contracts/proof_market/mina_state_verifier.sol
+++ contracts/proof_market/mina_state_verifier.sol
@@ -1,11 +1,29 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: Apache-2.0.
+//---------------------------------------------------------------------------//
+// Copyright (c) 2023 Haresh G <hgedia@nil.foundation>
+// Copyright (c) 2023 Amit Sagar <asagar@nil.foundation>
+// Copyright (c) 2023 Ilia Shirobokov <i.shirobokov@nil.foundation>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//---------------------------------------------------------------------------//
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import '@nilfoundation/evm-placeholder-verification/contracts/interfaces/verifier.sol';
+import "../interfaces/ICustomVerifier.sol";
 
 
-contract MinaStateVerifier is Ownable {
+contract MinaStateVerifier is ICustomVerifier, Ownable {
     address _verifier;
 
     address _base_gates;
